3255884fea136fe492361518934048044ac0c288
Alex Mumo <alexymumo2000@gmail.com>
2024-06-27 13:04:57 +0300

    Add db code

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"payd/internal/routes"
+	"payd/pkg/db"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.New()
+	db.ConnectDb()
 	router.Use(gin.Logger())
 	routes.UserRoutes(router)
 	routes.PaymentRoutes(router)
